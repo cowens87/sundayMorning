5ff662d76de0714283627d8271fd9dd231e9e04c
Cydnee Owens <cks410@yahoo.com>
2022-04-22 08:55:50 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// ~~~~~~~~~~~~~~~~~ global variables ~~~~~~~~~~~~~~~~~~~~~~~~
+let moneyCounter = 0
+
+// ~~~~~~~~~~~~~~~~~ event listeners ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// window.addEventListener("click", clickHandler);
+// window.addEventListener("load", displayRandomCover);
+// window.addEventListener("dblclick", deleteCover);
+
+// ~~~~~~~~~~~~~~~~~ functions ~~~~~~~~~~~~~~~~~~~~~~~~
